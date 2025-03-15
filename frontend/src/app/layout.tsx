2c7f5cc536ea7f3bf0b88fa153d2b33b9c78bf2c
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-03-16 00:15:05 +0530

    analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from "next";
 import { satoshi } from "./fonts/satoshi";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Donezo",
-  description: "Mail0 is a simple email client",
+  description: "Get weekly accountability emails inspired by tech visionaries to keep you focused and motivated",
 };
 
 export default function RootLayout({
@@ -17,6 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${satoshi.className} antialiased w-full`}>
         <Toaster position="top-center" />
+        <Script
+            defer
+            data-domain="https://donezo-theta.vercel.app/" // Replace with your domain
+            src="https://analytics-code.vercel.app/tracking-script.js"
+          />
         {children}
       </body>
     </html>
